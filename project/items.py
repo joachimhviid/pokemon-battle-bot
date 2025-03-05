fd57cc8ac3b46b03a5a88977b4424c776110ba4c
AndersBacon <Andth22@student.sdu.dk>
2025-03-05 16:58:23 +0100

    Commit dont work

--- project/items.py
+++ project/items.py
@@ -1,11 +1,11 @@
-import enum_items as ei
+from enum_items import Item
 from pokemon import Pokemon
 from pokemon_parser import parse_team
 
 
 def map_items():
     items_array = []
-    for i in ei.Item:
+    for i in Item:
         items_array.append(i)
     return items_array
 
@@ -19,34 +19,34 @@ def match_item(pokemon: Pokemon):
             if pokemon.stats["hp"] <= pokemon.stats["hp"] * 0.5:
                 pokemon.stats["hp"] += pokemon.stats["hp"] * 0.25
                 pokemon.held_item = "None" 
-            print(f"Used {pokemon_item} with {ei.Item.SITRUS_BERRY}")
+            print(f"Used {pokemon_item} with {Item.SITRUS_BERRY}")
         case "Focus Sash":
 #            if pokemon.stats["max_hp"] - damage == 0 or pokemon.stats["max_hp"] - damage ==  1:
 #                pokemon.stats["hp"] = 1
 #                pokemon.held_item = "None"
-                print(f"Used {pokemon_item} with {ei.Item.FOCUS_SASH}")
+                print(f"Used {pokemon_item} with {Item.FOCUS_SASH}")
         case "Life Orb":
-            print(f"Matched {pokemon_item} with {ei.Item.LIFE_ORB}")
+            print(f"Matched {pokemon_item} with {Item.LIFE_ORB}")
         case "Rocky Helmet":
-            print(f"Matched {pokemon_item} with {ei.Item.ROCKY_HELMET}")
+            print(f"Matched {pokemon_item} with {Item.ROCKY_HELMET}")
         case "Leftovers":
-            print(f"Matched {pokemon_item} with {ei.Item.LEFTOVERS}")
+            print(f"Matched {pokemon_item} with {Item.LEFTOVERS}")
         case "Booster Energy":
-            print(f"Matched {pokemon_item} with {ei.Item.BOOSTER_ENERGY}")
+            print(f"Matched {pokemon_item} with {Item.BOOSTER_ENERGY}")
         case "Covert Cloak":
-            print(f"Matched {pokemon_item} with {ei.Item.COVERT_CLOAK}")
+            print(f"Matched {pokemon_item} with {Item.COVERT_CLOAK}")
         case "Choice Specs":
-            print(f"Matched {pokemon_item} with {ei.Item.CHOICE_SPECS}")
+            print(f"Matched {pokemon_item} with {Item.CHOICE_SPECS}")
         case "Choice Scarf":
-            print(f"Matched {pokemon_item} with {ei.Item.CHOICE_SCARF}")
+            print(f"Matched {pokemon_item} with {Item.CHOICE_SCARF}")
         case "Safety Goggles":
-            print(f"Matched {pokemon_item} with {ei.Item.SAFETY_GOGGLES}")
+            print(f"Matched {pokemon_item} with {Item.SAFETY_GOGGLES}")
         case "Lum Berry":
-            print(f"Matched {pokemon_item} with {ei.Item.LUM_BERRY}")
+            print(f"Matched {pokemon_item} with {Item.LUM_BERRY}")
 
 
 if __name__ == "__main__":
-    team = parse_team('input/blastoise.json')
+    team = parse_team('blastoise')
     match_item(team[0])
 
     
