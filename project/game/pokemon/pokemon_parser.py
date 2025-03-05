bdde5814a9b4fec501441bd7d3988a4b89e36fd8
joachimhviid <joac1099@gmail.com>
2025-03-05 11:59:10 +0100

    Updated parser input arg

--- project/game/pokemon/pokemon_parser.py
+++ project/game/pokemon/pokemon_parser.py
@@ -2,18 +2,18 @@ from pokemon import Pokemon
 import json
 
 
-def parse_team(json_string: str) -> list[Pokemon]:
-    data = json.loads(json_string)
-    team = []
-    for pokemon in data:
-        team.append(
-            Pokemon(name=pokemon['name'], base_stats=pokemon['stats'], evs=pokemon['evs'], ivs=pokemon['ivs'], 
-                    nature=pokemon['nature'], level=pokemon['level'], moves=pokemon['moves'], types=pokemon['types'], 
-                    ability=pokemon['ability'], held_item=pokemon['held_item'])
-        )
-    return team
+def parse_team(file_name: str) -> list[Pokemon]:
+    with open(file_name, 'r') as file_stream:
+        data = json.loads(file_stream.read())
+        team = []
+        for pokemon in data:
+            team.append(
+                Pokemon(name=pokemon['name'], base_stats=pokemon['stats'], evs=pokemon['evs'], ivs=pokemon['ivs'],
+                        nature=pokemon['nature'], level=pokemon['level'], moves=pokemon['moves'], types=pokemon['types'],
+                        ability=pokemon['ability'], held_item=pokemon['held_item'])
+            )
+        return team
 
 
 if __name__ == "__main__":
-    with open('input/player_1.json', 'r') as file_stream:
-        parse_team(file_stream.read())
+    parse_team('input/player_1.json')
